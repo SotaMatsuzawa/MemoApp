e9a31a33cc4fe0064b19183e342abdfe7e0f6079
[SotaMatsuzawa] <[gglsouta2143@gmail.com]>
2021-02-06 01:03:43 +0900

    Remove console log

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -6,7 +6,7 @@ import firebase from 'firebase';
 
 import Button from '../components/Button';
 import Loading from '../components/Loading';
-import { trainslateErrors, translateErrors } from '../utils';
+import { translateErrors } from '../utils';
 
 export default function LogInScreen(props) {
   const { navigation } = props;
@@ -31,9 +31,7 @@ export default function LogInScreen(props) {
   function handlePress() {
     setLoading(true);
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        const { user } = userCredential;
-        console.log(user.uid);
+      .then(() => {
         navigation.reset({
           index: 0,
           routes: [{ name: 'MemoList' }],
